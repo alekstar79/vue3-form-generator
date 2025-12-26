258c98f028bdb063c967ddd976a498956e9cd09e
Aleksey Tarasenko <alekstar79@yandex.ru>
2025-12-26 16:17:50 +0400

    config updates

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
+    base: './vue3-form-generator/',
     outDir: 'dist',
     rollupOptions: {
       output: {
